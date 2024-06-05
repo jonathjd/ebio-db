703a55b8b810de2d76ec2973027760e02aca3675
Jonathan Dickinson <jon.dickinson@eclipsebio.com>
2024-06-05 09:32:27 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ebiodata',
+    version='1.0.0',
+    description='Test database',
+    author='Jonathan Dickinson',
+    author_email='jon.dickinson17@gmail.com',
+    license='MIT',
+    url='https://github.com/jonathjd/ebio-db',
+    packages=find_packages(),
+    install_requires=[
+        'numpy==1.26.4',
+        'pandas==2.2.2',
+        'python-dateutil==2.9.0.post0',
+        'pytz==2024.1',
+        'six==1.16.0',
+        'SQLAlchemy==2.0.30',
+        'typing_extensions==4.12.1',
+        'tzdata==2024.1',
+        'openpyxl==3.0'
+    ],
+    python_requires='>=3.8',
+)
